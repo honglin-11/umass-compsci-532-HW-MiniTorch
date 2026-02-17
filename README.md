b5a139a4b6760a5c3c691a169e284789c868b8d2
honglin-11 <honglin.wa@gmail.com>
2026-02-17 14:23:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+COMPSCI 532 - HW2 - MiniTorch Implementation
+First assigned Fall Semester of 2025.
+
+This repository contains the base code for the COMPSCI 532 assignment. See the course canvas HW2 instruction page for assignment details.
+
+Do not redistribute the base code in this repository, your solution code, or the project report. Please see the course academic integrity policy in the course syllabus for details, possible consequences, and explanation of this policy.
+
+This assignment is a modification of the MiniTorch Fundamentals Assignment. Original documentation can be found at the following links. All appropriate credit to the MiniTorch authors.
+
+* Docs: https://minitorch.github.io/
+
+* Overview: https://minitorch.github.io/module0/module0/
---
+++ minitorch/operators.py
@@ -0,0 +1,58 @@
+"""Collection of the core mathematical operators used throughout the code base."""
+
+import math
+
+# ## Task 0.1
+from typing import Callable, Iterable
+
+#
+# Implementation of a prelude of elementary functions.
+
+# Mathematical functions:
+# - mul
+def mul(x: float, y: float) -> float:
+    """$f(x, y) = x * y$"""
+    return x * y
+# TODO fill in the remaining mathematical operators. Use mul provided above as an example.
+# - id
+# - add
+# - neg
+# - lt
+# - eq
+# - max
+# - is_close
+# - sigmoid
+# - relu
+# - log
+# - exp
+# - log_back
+# - inv
+# - inv_back
+# - relu_back
+#
+# For sigmoid calculate as:
+# $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
+# For is_close:
+# $f(x) = |x - y| < 1e-2$
+
+
+# TODO: Implement for Task 0.1.
+
+
+# ## Task 0.3
+
+# Small practice library of elementary higher-order functions.
+
+# Implement the following core functions
+# - map
+# - zipWith
+# - reduce
+#
+# Use these to implement
+# - negList : negate all elemnts in a list using map
+# - addLists : add corresponding elements from two lists using zipWith
+# - sum: sum all elements in a list using reduce
+# - prod: tcalculate the product of all elements in a list using reduce
+
+
+# TODO: Implement for Task 0.3.
