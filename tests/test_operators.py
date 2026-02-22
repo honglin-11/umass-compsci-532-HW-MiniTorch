63ba53c64ab32dc3309b03af9a58c18eabd333cc
honglin-11 <honglin.wa@gmail.com>
2026-02-22 08:55:29 -0500

    Add unit tests for Task 2

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -99,24 +99,24 @@ def test_eq(a: float) -> None:
 
 
 @pytest.mark.task0_2
-@given(small_floats)
-def test_sigmoid(a: float) -> None:
+@given(small_floats, small_floats)
+def test_sigmoid(a: float, b: float) -> None:
     """Check properties of the sigmoid function, specifically
     * It is always between 0.0 and 1.0.
     * one minus sigmoid is the same as sigmoid of the negative
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert_close(sigmoid(0.0), 0.5)
+    assert sigmoid((a if a > b else b)) >= sigmoid((b if a > b else a))
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert not (lt(a, b) and lt(b, c)) or lt(a, c)
 
 
 @pytest.mark.task0_2
@@ -125,26 +125,24 @@ def test_symmetric(a: float, b: float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
     assert_close(mul(a,b), mul(b,a))
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
-def test_distribute() -> None:
+def test_distribute(a: float, b: float, c: float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(a, add(b, c)), add(mul(a, b), mul(a, c)))
 
 
 @pytest.mark.task0_2
 @given(small_floats)
-def test_other() -> None:
+def test_other(a: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    # TODO: See if we need to get more creative with this
+    assert_close(abs(a), abs(neg(a)))
 
 
 # ## Task 0.3  - Higher-order functions
